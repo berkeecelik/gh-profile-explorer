62ccaa7e7a66f20269331709aad071f26c026e01
Berke Celik <celikberke@outlook.com>
2022-04-19 00:59:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="Hipo-logo" alt="logo" />
+        <h1 id="title">Github Profile Explorer</h1>
+        <div>
+          <form>
+            <input
+              type="text"
+              id="nameInput"
+              placeholder="Type username"
+            ></input>
+          </form>
+        </div>
       </header>
     </div>
   );
